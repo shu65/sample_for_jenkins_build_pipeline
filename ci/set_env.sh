652f16cd3d699e64dafad9c2dff46a7afb62f7f8
shu <shu65>
2017-03-20 17:36:46 +0900

    fixed image tag

--- ci/set_env.sh
+++ ci/set_env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export IMAGE_NAME="sample_for_jenkins_build_pipeline"
-export IMAGE_TAG=`git show --pretty=format:'%ad-%H' --date 'format:%Y%m%dT%H%M%S' | head -n 1`
+export IMAGE_TAG=`git show --pretty=format:'%ad-%H' --date=short | head -n 1`
 export IMAGE=${IMAGE_NAME}:${IMAGE_TAG}
