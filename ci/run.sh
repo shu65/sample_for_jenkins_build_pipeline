010f5f0f13d27cace69887c23ec0c0efb73e1c15
shu <shu65>
2017-03-20 17:01:52 +0900

    fixed run command

--- ci/run.sh
+++ ci/run.sh
@@ -4,7 +4,7 @@ if [ ! -n "${IMAGE+1}" ]
 then
     echo "Error: you should run '. set_env.sh'."
     exit 1
-
 fi
 
-docker run --rm -it ${IMAGE} bash
+docker run --rm -it ${IMAGE} /sample_for_jenkins_build_pipeline/hello_world
+
